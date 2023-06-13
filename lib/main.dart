156b0f202897a8867445c2ac0c86cbfc89cdb941
alish <alishojib12@gmail.com>
2023-06-14 01:44:15 +0600

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_app/colors.dart';
+import 'package:whatsapp_app/firebase_options.dart';
 import 'package:whatsapp_app/screens/mobile_layout_screen.dart';
 import 'package:whatsapp_app/screens/web_layout_screen.dart';
 import 'package:whatsapp_app/utils/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
