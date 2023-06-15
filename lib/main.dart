26bafd7c5f0eee5e2fb5dc6dee67ce5bc56448d8
alish <alishojib12@gmail.com>
2023-06-15 16:52:15 +0600

    firebasee phone auth ripository plus landing ui with page route initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_app/colors.dart';
+import 'package:whatsapp_app/features/landing/screens/landing_screen.dart';
 import 'package:whatsapp_app/firebase_options.dart';
+import 'package:whatsapp_app/router.dart';
 import 'package:whatsapp_app/screens/mobile_layout_screen.dart';
 import 'package:whatsapp_app/screens/web_layout_screen.dart';
 import 'package:whatsapp_app/utils/responsive_layout.dart';
@@ -20,15 +22,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Whatsapp UI',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: backgroundColor,
-      ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileLayoutScreen(),
-        webScreenLayout: WebLayoutScreen(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Whatsapp UI',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: backgroundColor,
+        ),
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: const LandingScreen()
+        // const ResponsiveLayout(
+        //   mobileScreenLayout: MobileLayoutScreen(),
+        //   webScreenLayout: WebLayoutScreen(),
+        // ),
+        );
   }
 }
