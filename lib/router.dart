7d4139bc47d3ede3d3032ed7cabebace6743e5d2
alish <alishojib12@gmail.com>
2023-06-24 22:25:02 +0600

    select contacts ui page & contact provider plus repository half way

--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,7 @@ import 'package:whatsapp_app/common/widgets/error.dart';
 import 'package:whatsapp_app/features/auth/screens/login_screen.dart';
 import 'package:whatsapp_app/features/auth/screens/otp_screen.dart';
 import 'package:whatsapp_app/features/auth/screens/user_information_screen.dart';
+import 'package:whatsapp_app/features/select_contacts/screens/select_contact_screen.dart';
 // import 'package:whatsapp_app/features/group/screens/create_group_screen.dart';
 // import 'package:whatsapp_app/features/select_contacts/screens/select_contacts_screen.dart';
 // import 'package:whatsapp_app/features/chat/screens/mobile_chat_screen.dart';
@@ -27,10 +28,10 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (context) => const UserInformationScreen(),
       );
-//     // case SelectContactsScreen.routeName:
-//     //   return MaterialPageRoute(
-//     //     builder: (context) => const SelectContactsScreen(),
-//     //   );
+    case SelectContactsScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const SelectContactsScreen(),
+      );
 //     // case MobileChatScreen.routeName:
 //     //   final arguments = settings.arguments as Map<String, dynamic>;
 //     //   final name = arguments['name'];
--- lib/screens/mobile_layout_screen.dart
+++ lib/screens/mobile_layout_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_app/colors.dart';
+import 'package:whatsapp_app/features/select_contacts/screens/select_contact_screen.dart';
 import 'package:whatsapp_app/widgets/contacts_list.dart';
 
 class MobileLayoutScreen extends StatelessWidget {
@@ -55,7 +56,9 @@ class MobileLayoutScreen extends StatelessWidget {
         ),
         body: const ContactsList(),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pushNamed(context, SelectContactsScreen.routeName);
+          },
           backgroundColor: tabColor,
           child: const Icon(
             Icons.comment,
